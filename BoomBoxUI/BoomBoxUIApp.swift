b7db2cca67cadbe483ce8e6680639c0513261513
Ronan <ronan@ronanfuruta.com>
2023-08-03 21:43:09 -0400

    Initial Commit

---
+++ BoomBoxUI/BoomBoxUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoomBoxUIApp.swift
+//  BoomBoxUI
+//
+//  Created by Ronan Furuta on 8/3/23.
+//
+
+import SwiftUI
+
+@main
+struct BoomBoxUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
